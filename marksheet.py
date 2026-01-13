7b5bc85b85e62d5eafd6c524b01733b2c2cb195e
Aaftab I gori <goriaaftab87@gmail.com>
2026-01-13 23:16:32 +0530

    added something

--- marksheet.py
+++ marksheet.py
@@ -1,16 +1,32 @@
 s1=int(input('Enter the marks of subject 1: '))
 s2=int(input('Enter the marks of subject 2: '))
 s3=int(input('Enter the marks of subject 3: '))
+s4=int(input('Enter the marks of subject 4: '))
+s5=int(input('Enter the marks of subject 5: '))
+s6=int(input('Enter the marks of subject 6: '))
+s7=int(input('Enter the marks of subject 7: '))
 
-subject=[s1,s2,s3]
+subject=[s1,s2,s3,s4,s5,s6,s7]
 
-tot_marks=s1+s2+s3
-percentage=tot_marks/len(subject)
+tot_marks=s1+s2+s3+s4+s5+s6+s7
+pr=tot_marks/len(subject)
 
 print('total marks is: ',tot_marks)
-print('percentage is: ',percentage)
+print('percentage is: ',pr)
 
-if percentage>=40:
-    print('Pass')
+if pr<33:
+    print('F')
+elif pr>=90:
+    print('A+')
+elif pr>=80:
+    print('A')
+elif pr>=70:
+    print('B')
+elif pr>=60:
+    print('C')
+elif pr>=50:
+    print('D')
+elif pr<=33:
+    print('E')
 else:
-    print('Fail')
+    print('Invalid')
